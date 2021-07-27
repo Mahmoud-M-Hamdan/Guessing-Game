c77e94a70e76ef60de2295a1f4cfb29786e6d1eb
Mahmoud-M-Hamdan <eng.mech.hamdan@gmail.com>
2021-07-27 16:22:58 +0300

    adding 2 q

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var ra=0;
+
 let nameuser=prompt('hey there , provide me with ur name please ')
 alert('Hey '+ nameuser + ' i hope u fine , before let u in i will ask u 5 quistions and u should answer it with yes or no only , it is aquestions about me u should guest it , dont fear and be ready , then leet goo' );
 
@@ -9,7 +11,9 @@ let food=prompt('Do u think my fav food is mansaf ?').toLocaleLowerCase();
 switch (food) {
     case 'yes':
     case 'y':
-  alert('right answer')
+  alert('right answer');
+  ra++;
+
  // console.log(food, 'is right answer')
       break;
       case 'no':
@@ -28,18 +32,20 @@ switch (field) {
       case 'no':
       case 'n':
       alert('right answer')
+      ra++;
      // console.log(field, 'is right answer')
       break;
     default:
         alert('the answer should be just yes or no');
 }
 
-let x=prompt('Do u love me ?').toLocaleLowerCase();
-switch (x) {
+let xm=prompt('Do u love me ?').toLocaleLowerCase();
+switch (xm) {
   case 'yes':
   case 'y':
   alert('right answer')
-  // console.log(x, 'is right answer')
+  ra++;
+  // console.log(xm, 'is right answer')
       break;
       case 'no':
       case 'n':
@@ -58,6 +64,7 @@ switch (grade) {
       case 'no':
       case 'n':
       alert('good boy , it is right')
+      ra++;
      // console.log(grade, 'is right answer')
       break;
     default:
@@ -69,6 +76,7 @@ switch (hobby) {
   case 'yes':
   case 'y':
   alert('right answer')
+  ra++;
   // console.log(hobby, 'is right answer')
       break;
       case 'no':
@@ -78,7 +86,49 @@ switch (hobby) {
     default:
         alert('the answer should be just yes or no');
 }
-alert('Good answers '+ nameuser+' , please join to my website ')
+
+
+for ( let i=0 ; i<4 ; i++){
+let num=Number(prompt('U should guess a number between 0 and 20 , u just have 4 attempt to gues the right number'));
+if (num <8){
+alert('too low')
+
+}
+ else if (num >8)
+{
+alert('too high');
+}
+else{
+  alert('Correct Answer =)');
+  ra++;
+  i=4;
+}
+
+}
+alert('the Correct Answer is 8');
+let x=[3,9,15,20,100]
+for(let y=0 ; y<6 ; y++){
+  let guessNum=Number(prompt('U should guess one of the right numbers , u just have 6 attempt to gues the right number'))
+  
+  switch(guessNum)
+  {
+    case x[0]:
+    case x[1]:
+    case x[2]:
+    case x[3]:
+    case x[4]:
+      alert('correct answer');
+      ra++;
+      y=6;
+      break;
+    default:
+    alert('wrong answer');
+  }
+}
+alert('all the correct answers are : '+ x );
+alert('Good answers '+ nameuser+' your right answers are ' + ra);
+
+
 
 
 
